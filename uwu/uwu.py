04a0a424753722fd7754c746087c901ce5739b26
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-20 16:30:27 -0500

    Update uwu.py

--- uwu/uwu.py
+++ uwu/uwu.py
@@ -254,7 +254,7 @@ class uwu(commands.Cog, IDConverter):
         if member == ctx.author:
             return await ctx.send("A-are you feeling lonely? ;c")
         else:
-            patbed = discord.Embed(description=msg.format(mem=member.id, auth=auth.id), color=discord.Color(rand(self.clist)))
+            patbed = discord.Embed(description=msg.format(mem=member.display_name, auth=auth.display_name), color=discord.Color(rand(self.clist)))
             patbed.set_image(url=rand(self.patg))
             await ctx.send(embed=patbed)
         #Message Sending
