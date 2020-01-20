73e8dc322a6c763904ea99a13e9d58893deb19f6
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-20 17:01:15 -0500

    Update uwu.py

--- uwu/uwu.py
+++ uwu/uwu.py
@@ -33,10 +33,10 @@ patg = [
 ]
 
 patm = [
-    "**{auth}** pats **{mem}** on the head ^-^",
-    "**{auth}** pattie cakes **{mem}’s** head 0-0",
-    "**{mem}** gets a lucky 'ol pat on the head from **{auth}** UwU",
-    "**{mem}** recieves a pat from **{auth}** =-=",
+    "{auth} pats **{mem} on the head ^-^",
+    "{auth} pattie cakes {mem}’s head 0-0",
+    "{mem} gets a lucky 'ol pat on the head from {auth} UwU",
+    "{mem}** recieves a pat from {auth} =-=",
 ]
 
 kissg = [
