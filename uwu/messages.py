562528fe8aa22ce970d8f5846abfc9701bd03064
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-20 19:03:32 -0500

    Update messages.py

--- uwu/messages.py
+++ uwu/messages.py
@@ -4,3 +4,13 @@ pat_msg = [
     "{mem} gets a lucky 'ol pat on the head from {auth} UwU",
     "{mem} recieves a pat from {auth} =-=",
 ]
+
+self_pat_msg = [
+    "oh noo ;-; I'll pat you {auth}..."
+    "There there {auth}..."
+]
+
+excited_msg = [
+    "{auth} is Excited!"
+]
+
