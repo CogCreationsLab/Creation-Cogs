576c75c435431357372ff8bb4da698a72dcb8f53
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-20 19:00:58 -0500

    Update uwu.py

--- uwu/uwu.py
+++ uwu/uwu.py
@@ -67,7 +67,7 @@ class uwu(commands.Cog):
         else:
             excitedbed = discord.Embed(color=discord.Color(rand(self.clist)))
             excitedbed.set_image(url=rand(self.excited_gif))
-            excitedbed.set_author(name=msg.format(mem=member.display_name, auth=auth.display_name), icon_url=ctx.author.avatar_url)
+            excitedbed.set_author(name=msg.format(auth=auth.display_name), icon_url=ctx.author.avatar_url)
             await ctx.send(embed=excitedbed)
         #Pat Command     
             
