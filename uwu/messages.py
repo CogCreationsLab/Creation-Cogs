bd2dffc431f443e104a6d0bfbd4eb8f9a7f03058
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-20 20:26:17 -0500

    Update messages.py

--- uwu/messages.py
+++ uwu/messages.py
@@ -6,16 +6,16 @@ pat_msg = [
 ]
 
 self_pat_msg = [
-    "oh noo ;-; I'll pat you {auth}..."
+    "oh noo ;-; I'll pat you {auth}...",
     "There there {auth}..."
 ]
 
 cuddle_msg = [
-    "{auth} cuddles {mem}~ adorable!"
+    "{auth} cuddles {mem}~ adorable!",
     "{auth} cuddles {mem}~~ kyaaaa!!"
 ]
 
 self_cuddle_msg = [
-    "D-do you need a hug {auth}...?"
+    "D-do you need a hug {auth}...?",
     "oh noo ;-; I'll cuddle you {auth}..."
 ]
