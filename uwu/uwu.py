68f4d2ea6705f30a5a060d09d18e351e4fdd73ea
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-21 22:55:14 -0500

    Update uwu.py

--- uwu/uwu.py
+++ uwu/uwu.py
@@ -117,12 +117,11 @@ class uwu(commands.Cog):
     async def excited():
         auth = ctx.author
         msg = rand(self.excited_msg)
-        ctx.send( 
-        excitedbed = discord.Embed(
-        color=discord.Color(rand(self.clist)))
+        excitedbed = discord.Embed(color=discord.Color(rand(self.clist)))
             
         excitedbed.set_image(url=rand(self.excited_gif))
         excitedbed.set_author(name=msg.format(auth=auth.display_name), icon_url=ctx.author.avatar_url)
+        
         await ctx.send(embed=excitedbed)
 
 #################
