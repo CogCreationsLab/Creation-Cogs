6d764a459f7e242d2ceebdeb255c17baecbe2aed
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-02-01 03:15:22 -0500

    Update uwu.py

--- uwu/uwu.py
+++ uwu/uwu.py
@@ -246,9 +246,17 @@ class uwu(commands.Cog):
         if member == ctx.author:
             wavebed = discord.Embed(color=discord.Color(rand(self.clist)))
             wavebed.set_image(url=rand(self.wave_gif))
-            wavebed.set_author(name=msg.format(mem=member.display_name, auth=auth.display_name), icon_url=ctx.author.avatar_url)
+            wavebed.set_author(name=msg.format(auth=auth.display_name), icon_url=ctx.author.avatar_url)
             
             await ctx.send(embed=wavebed)
+            
+        else:
+            wavebed = discord.Embed(color=discord.Color(rand(self.clist)))
+            wavebed.set_image(url=rand(self.wave_gif))
+            wavebed.set_author(name=msg.format(mem=member, auth=auth), icon_url=ctx.author.avatar_url)
+            
+            await ctx.send(embed=wavebed)
+            
         
 ################        
 #Commands To Add#
