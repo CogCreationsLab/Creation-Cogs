a92baf4918adfeef1af178aa7469021234ca4ffd
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-31 23:53:06 -0500

    Update uwu.py

--- uwu/uwu.py
+++ uwu/uwu.py
@@ -254,6 +254,8 @@ class uwu(commands.Cog):
         msg = rand(self.wave_msg)
         self_msg = rand(self.selfwave_msg)
         
+        if member == ctx.author:
+            
         wavebed = discord.Embed(color=discord.Color(rand(self.clist)))
         wavebed.set_image(url=rand(self.wave_gif))
         wavebed.set_author(name=self_msg.format(auth=auth.display_name), icon_url=ctx.author.avatar_url)
